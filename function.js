933492018ec1b550007852828ceca5f84909feb6
Syvit <collin.trumpler@gmail.com>
2022-10-09 11:20:47 -0400

    created hover effect

--- function.js
+++ function.js
@@ -1,11 +1,18 @@
 
 function createGrid(){
-for (let i = 0; i<256; i++){
-    const gridBox = document.createElement('div');
-    gridBox.className = 'gridBox';
-    gridBox.id = i + 1;
- 
-    document.querySelector('.gridContainer').appendChild(gridBox);
-}
+    for (let i = 0; i<256; i++){
+        const gridBox = document.createElement('div');
+        gridBox.className = 'gridBox';
+        gridBox.id = i + 1;
+        document.querySelector('.gridContainer').appendChild(gridBox);
+        
+        gridBox.onmouseover = function(e){
+            e.target.style.borderColor = 'red';
+            e.target.style.backgroundColor = 'red';
+        }
+        
+    }
 };
+
 createGrid();
+
