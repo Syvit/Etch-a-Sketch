d51c475dabfafa19a0c3cc85005158903360c0bb
Syvit <collin.trumpler@gmail.com>
2022-10-14 18:48:14 -0400

    Added grid size selection and variability. for simplicity I changed to a three-option selection system. grid changes scale for each selection to conform to boundaries, this is hard-coded in which is fine for a project, however if this were a professional project I would need to change that to something more variable. I also did some basic styling.

--- function.js
+++ function.js
@@ -1,18 +1,77 @@
+const gridSelect = document.querySelectorAll('button'); 
+const container = document.querySelector('.gridContainer');
+const div = document.querySelectorAll('div');
 
-function createGrid(){
-    for (let i = 0; i<256; i++){
-        const gridBox = document.createElement('div');
-        gridBox.className = 'gridBox';
-        gridBox.id = i + 1;
-        document.querySelector('.gridContainer').appendChild(gridBox);
-        
-        gridBox.onmouseover = function(e){
-            e.target.style.borderColor = 'red';
-            e.target.style.backgroundColor = 'red';
+
+function gridGen(gridNum, padSize){
+    for (let i = 0; i < gridNum; i++){
+        const row = document.createElement('div');
+        row.className = 'row';
+        for(let x = 1; x<=gridNum; x++){
+            const cell = document.createElement('div');
+            cell.className = 'gridCell';
+            // cell.innerHTML = (i * gridNum) + x;
+            row.appendChild(cell);         
+            row.onmouseover = function(e){
+                e.target.style.backgroundColor = 'black';
+            }
+            cell.style.padding =`${(padSize)}em`;
         }
-        
+        container.appendChild(row);
     }
-};
+    
 
-createGrid();
+}
 
+function gridSize(){
+    gridSelect.forEach((button) => 
+        button.addEventListener('click', (e)=> {   
+            gridChoice(button.id);
+        })
+    )};
+
+    function gridChoice(size){
+        if (size == 'small') {
+            gridGen(16,0.915);
+                gridSelect.forEach((button) =>{
+                    button.remove();
+                });
+            const reset = document.createElement('button');
+            reset.textContent = 'Reset';
+            document.querySelector('#gridCount').append(reset);
+            
+            reset.addEventListener('click', () => {
+                location.reload();
+            })
+        }
+        else if (size == 'medium'){
+            gridGen(32,0.425);
+
+                gridSelect.forEach((button) =>{
+                    button.remove();
+                });
+            const reset = document.createElement('button');
+            reset.textContent = 'Reset';
+            document.querySelector('#gridCount').append(reset);
+
+            reset.addEventListener('click', () => {
+                location.reload();
+        })
+        }
+        else if (size == 'large'){
+            gridGen(64,0.181);
+                gridSelect.forEach((button) =>{
+                    button.remove();
+                });
+            const reset = document.createElement('button');
+            reset.textContent = 'Reset';
+            document.querySelector('#gridCount').append(reset);
+            
+            reset.addEventListener('click', () => {
+                location.reload();
+        })
+        }
+    };
+
+    gridSize();
+    gridChoice();
